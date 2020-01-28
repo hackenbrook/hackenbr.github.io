10a1efbbe709cd4d6770badc3378f8c38be95900
Mr. Hackenbrook <32421031+hackenbr@users.noreply.github.com>
2020-01-28 16:50:50 -0700

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -1,13 +1,17 @@
 /* Set the width of the side navigation to 250px */
-
 function myFunction(x) {
-  	x.classList.toggle("change");
- 	var side = document.getElementById("mySidenav").style.width;
+	x.classList.toggle("change");
+	x.classList.toggle("addLinks");
+	var side = document.getElementById("mySidenav").style.width;
 	if (side == 0 || side == "0px") {
-		document.getElementById("mySidenav").style.width = "500px";
+
+		document.getElementById("mySidenav").style.width = "400px";
+
+
 	
 	}
 	else {
 		document.getElementById("mySidenav").style.width = "0";
 	}
+
 }
