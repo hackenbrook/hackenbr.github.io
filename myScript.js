38dd1d43871d72f2a080423e878e97cbfb83052c
Mr. Hackenbrook <32421031+hackenbr@users.noreply.github.com>
2020-03-03 10:12:31 -0700

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -7,6 +7,7 @@ function myFunction(x) {
 
 		document.getElementById("mySidenav").style.width = "400px";
 		document.getElementById("mySidenav").style.zIndex = "1";
+		document.getElementsByClassName("change bar1").style.zIndex = "2";
 	
 		
 
