a030db7c5912e360233212828985f919e39e9940
Mr. Hackenbrook <32421031+hackenbr@users.noreply.github.com>
2020-03-03 09:39:25 -0700

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -6,8 +6,7 @@ function myFunction(x) {
 	if (side == 0 || side == "0px") {
 
 		document.getElementById("mySidenav").style.width = "400px";
-		document.getElementsByClassName("bar3").style.z-index = "1";
-		document.getElementsByClassName("bar1").style.z-index = "1";
+
 	}
 	else {
 		document.getElementById("mySidenav").style.width = "0";
